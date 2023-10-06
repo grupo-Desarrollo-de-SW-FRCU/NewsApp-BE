514d652304e6bcb71b569c963ac44e054880a35d
Aaron <aaronchiappella@gmail.com>
2023-10-06 16:42:50 -0300

    Cambios a atributos Article

--- src/NewsApp.Application.Contracts/Articles/ArticleDto.cs
+++ src/NewsApp.Application.Contracts/Articles/ArticleDto.cs
@@ -8,12 +8,16 @@ namespace NewsApp.Articles
 {
     public class ArticleDto : AuditedEntityDto<Guid>
     {
-        public string Title {get; set; }   
-        public string Description {get; set; }
-        public string Content {get; set; } 
-        public string Url {get; set; }
-        public string UrlToImage { get; set; }
-        public IdiomEnum Idiom {  get; set; }
+        
+      //public Source Source { get; set; } DESCOMENTAR CUANDO SOURCE ESTE CREADA
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Url { get; set; }
+        public string? UrlToImage { get; set; }
+        public IdiomEnum? Idiom { get; set; } //no reconoce la clase
 
+        public DateTime PublishedAt { get; set; }
+        public string Content { get; set; }
     }
 }
--- src/NewsApp.Domain/Articles/Article.cs
+++ src/NewsApp.Domain/Articles/Article.cs
@@ -10,12 +10,20 @@ namespace NewsApp.Articles
 {
     public class Article : Entity<Guid>
     {//autor titulo descripcion url urltoimage publishedat content
+       
+        //public Source Source { get; set; } DESCOMENTAR CUANDO SOURCE ESTE CREADA
+        public string Author { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public string Content { get; set; }
         public string Url { get; set; }
-        public string UrlToImage { get; set; }
-        public IdiomEnum Idiom { get; set; } //no reconoce la clase
+        public string? UrlToImage { get; set; }
+        public IdiomEnum? Idiom { get; set; } //no reconoce la clase
+
+        public DateTime PublishedAt {get; set; }
+        public string Content { get; set; }
+
+
+        //source; publishAT; source
     }
 }
 
