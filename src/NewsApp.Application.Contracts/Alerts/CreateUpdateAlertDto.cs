a007ee492d70e5513887cf1082223cf7958e788b
Facundo Dun <facundotomasdun10@gmail.com>
2023-10-17 12:38:38 -0300

    Agregar CreateUpdate a searchs y alerts.

---
+++ src/NewsApp.Application.Contracts/Alerts/CreateUpdateAlertDto.cs
@@ -0,0 +1,26 @@
+﻿using Abp.Authorization.Users;
+using NewsApp.Searchs;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace NewsApp.Alerts
+{ 
+public class CreateUpdateAlertDto
+{
+
+        [Required]
+        public bool Active { get; set; }
+        [Required]
+        public DateTime CreatedDate { get; set; }
+        [Required]
+        public string SearchText { get; set; }
+        [Required]
+        public AbpUserBase User { get; set; }
+        [Required]
+        public SearchDto Search { get; set; }
+        [Required]
+        public Guid SearchOfAlertId { get; set; }
+        [Required]
+        public string Name { get; set; }
+}
+}
---
+++ src/NewsApp.Application.Contracts/Searchs/CreateUpdateSearchDto.cs
@@ -0,0 +1,35 @@
+﻿using Abp.Authorization.Users;
+using System;
+using System.ComponentModel.DataAnnotations;
+using NewsApp.Articles;
+using NewsApp.Alerts;
+using NewsApp.Failures;
+using System.Collections.Generic;
+
+namespace NewsApp.Searchs
+{
+    public class CreateUpdateAlertDto
+    {
+
+        [Required]
+        public string SearchString { get; set; }
+        [Required]
+        public DateTime StartDateTime { get; set; }
+        [Required]
+        public int ResultsAmount { get; set; }
+        [Required]
+        public DateTime EndDateTime { get; set; }
+        [Required]
+        public FailureDto? Failure { get; set; }
+        [Required]
+
+        public AlertDto? Alert { get; set; }
+        [Required]
+
+        public AbpUserBase User { get; set; }
+        [Required]
+
+        public ICollection<ArticleDto> Articles { get; set; }
+
+    }
+}
