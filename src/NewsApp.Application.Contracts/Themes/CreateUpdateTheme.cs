67178b3238054d4920601a64a506de9ae723b717
PC-CHAPI <PC-CHAPI@DESKTOP-8LQPAVN>
2023-10-17 18:48:31 -0300

    agregue CreateUpdateTthem

---
+++ src/NewsApp.Application.Contracts/Themes/CreateUpdateTheme.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace NewsApp.Themes
+{
+    internal class CreateUpdateTheme
+    {
+
+        [Required]
+        [StringLength(100)]
+        public string name { get; set; }
+    }
+}
