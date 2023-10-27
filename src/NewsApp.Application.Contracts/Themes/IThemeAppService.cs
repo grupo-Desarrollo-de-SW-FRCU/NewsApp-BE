73c1b4168ca300b82b4b112808d37ae56f3d32f9
Tadeo-Lopez <tadeomat1@gmail.com>
2023-10-27 09:34:32 -0300

    Metodos agregados en IThemeAppService y metodo RemoveArticle creado

--- src/NewsApp.Application.Contracts/Themes/IThemeAppService.cs
+++ src/NewsApp.Application.Contracts/Themes/IThemeAppService.cs
@@ -1,14 +1,23 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Abp.Application.Services;
+using NewsApp.Articles;
+using static Castle.MicroKernel.ModelBuilder.Descriptors.InterceptorDescriptor;
 
 namespace NewsApp.Themes
 {
-    public interface IThemeAppService : 
+    public interface IThemeAppService :
         ICrudAppService< //Defines CRUD methods
         ThemeDto, //Used to show books
         Guid, //Primary key of the book entity
         CreateUpdateThemeDto> //Used to create/update a book
     {
+        public Task AddTheme(Guid id, ThemeDto otherTheme);
 
+        public Task RemoveTheme(Guid themeId, Guid themeToRemoveId);
+
+        public Task AddArticle(Guid id, ArticleDto article);
+
+        public Task RemoveArticle(Guid themeId, Guid articleToRemoveId);
     }
 }
--- src/NewsApp.Application/Themes/ThemeAppService.cs
+++ src/NewsApp.Application/Themes/ThemeAppService.cs
@@ -5,7 +5,7 @@ using Abp.Application.Services;
 using NewsApp.Articles;
 namespace NewsApp.Themes;
 
-public class ThemeAppService : CrudAppService<Theme, ThemeDto, Guid,CreateUpdateThemeDto>, IThemeAppService
+public class ThemeAppService : CrudAppService<Theme, ThemeDto, Guid, CreateUpdateThemeDto>, IThemeAppService
 {
     private Abp.Domain.Repositories.IRepository<Theme, Guid> _repository;
 
@@ -41,6 +41,20 @@ public class ThemeAppService : CrudAppService<Theme, ThemeDto, Guid,CreateUpdate
         var theme = await _repository.GetAsync(id);
         theme.Articles.Add(article);
         _ = await _repository.UpdateAsync(theme);
+    }
+
+    public async Task RemoveArticle(Guid themeId, Guid articleToRemoveId)
+    {
+        var theme = await _repository.GetAsync(themeId);
+        if (theme.Articles != null)
+        {
+            var articleToRemove = theme.Articles.FirstOrDefault(t => t.Id == articleToRemoveId);
+            if (articleToRemove != null)
+            {
+                theme.Articles.Remove(articleToRemove);
+                await _repository.UpdateAsync(theme);
+            }
+        }
     }
 }
 
