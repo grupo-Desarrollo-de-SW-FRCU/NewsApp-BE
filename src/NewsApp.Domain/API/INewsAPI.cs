0e5a2b49a034c446c160c049a8aa64270bf6867b
Aaron <aaronchiappella@gmail.com>
2023-10-27 20:32:39 -0300

    constructores alertas agregados con inconveniente de clases compuestas

--- src/NewsApp.Domain/API/INewsAPI.cs
+++ src/NewsApp.Domain/API/INewsAPI.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 public interface INewsAPI
 {
-    Task<string> getNews(string LanguageIntCode, int? amountNews);
+    Task<string> getNews( string LanguageIntCode, int? amountNews);
 
     //ACA SE DEFINEN METODOS DE LA API QUE SON RESUELTOS EN EL HANDLER
 }
