f64880d2e47d04b78b8c1040fa8df3593099a6b6
Tadeo-Lopez <tadeomat1@gmail.com>
2023-11-01 23:22:50 -0300

    Alerts App Service

--- src/NewsApp.Application/Alerts/AlertAppService.cs
+++ src/NewsApp.Application/Alerts/AlertAppService.cs
@@ -5,15 +5,38 @@ using System.Text;
 using System.Threading.Tasks;
 using Volo.Abp.Application.Services;
 using Volo.Abp.Domain.Repositories;
+using Volo.Abp.Identity;
+using Volo.Abp.Users;
+using static Volo.Abp.Identity.Settings.IdentitySettingNames;
 
 namespace NewsApp.Alerts
 {
     public class AlertAppService : CrudAppService<Alert, AlertDto, Guid>, IAlertAppService
     {
-        public AlertAppService(IRepository<Alert, Guid> repository)
+        private readonly IRepository<IdentityUser, Guid> _userRepository;
+        public AlertAppService(IRepository<Alert, Guid> repository, IRepository<IdentityUser, Guid> userRepository)
             : base(repository)
         {
-
+            _userRepository = userRepository;
         }
+
+        // falta definir Searches en user
+
+        //public async Task CheckAlerts(Guid userId)
+        //{
+        //    var user = await _userRepository.GetAsync(userId);
+        //    foreach (var search in user.Searches)
+        //    {
+        //        if (search.Alert != null)
+        //        {
+        //            var results = await ExecuteSearch(search.SearchString);
+        //            if (results.Any())
+        //            {
+        //                await _notificationService.NotifyUser(userId);
+        //                await DeleteAlert(search.Alert.Id);
+        //            }
+        //        }
+        //    }
+        //}
     }
 }
