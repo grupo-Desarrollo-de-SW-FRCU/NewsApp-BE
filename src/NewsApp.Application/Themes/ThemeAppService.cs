3687fcd5bdb5d6a54670167ff4a3cdf7a8c449d6
Tadeo-Lopez <tadeomat1@gmail.com>
2023-11-14 10:38:21 -0300

    Metodos CreateThemeAsync y UpdateThemeAsync, cambio el IdentityUser de Theme por Guid UserId, debido a que sino se presentan errores con el createUpdateThemeDto

--- src/NewsApp.Application/Themes/ThemeAppService.cs
+++ src/NewsApp.Application/Themes/ThemeAppService.cs
@@ -27,7 +27,7 @@ namespace NewsApp.Themes
 
         public async Task<ThemeDto> GetThemesAsync(Guid id)
         {
-            var queryable = await _repository.WithDetailsAsync(x => x.User);
+            var queryable = await _repository.WithDetailsAsync(x => x.UserId);
 
             var query = queryable.Where(x => x.Id == id);
 
@@ -35,5 +35,54 @@ namespace NewsApp.Themes
 
             return ObjectMapper.Map<Theme, ThemeDto>(theme);
         }
+
+        public async Task<ThemeDto> CreateThemeAsync(CreateUpdateThemeDto input)
+        {
+            var theme = new Theme
+            {
+                Name = input.Name,
+                KeyWords = input.KeyWordsToAdd,    
+                UserId = input.UserId,
+            };
+
+            theme = await _repository.InsertAsync(theme);
+
+            return ObjectMapper.Map<Theme, ThemeDto>(theme);
+        }
+
+        public async Task<ThemeDto> UpdateThemeAsync(Guid id, CreateUpdateThemeDto input)
+        {
+            var themeToUpdate = await _repository.GetAsync(id);
+
+            if (themeToUpdate != null)
+            {
+                // Actualizar el nombre del tema
+                themeToUpdate.Name = input.Name;
+
+                // Agregar nuevas palabras clave a la lista existente
+                foreach (var keywordToAdd in input.KeyWordsToAdd)
+                {
+                    themeToUpdate.KeyWords.Add(keywordToAdd);
+                }
+
+                // Eliminar palabras clave que aparecen en la lista de palabras clave a eliminar
+                foreach (var keywordToRemove in input.KeyWordsToRemove)
+                {
+                    themeToUpdate.KeyWords.Remove(keywordToRemove);
+                }
+
+                await _repository.UpdateAsync(themeToUpdate);
+
+                return ObjectMapper.Map<Theme, ThemeDto>(themeToUpdate);
+            }
+            else
+            {
+                // Manejar el caso en que el tema no se encuentre
+                throw new ArgumentException($"Theme with id {id} not found.");
+            }
+        }
+
+
+
     }
 }
