8466b9fb886f0bd37079eebf9ab0ebeead3d8396
Tadeo-Lopez <tadeomat1@gmail.com>
2023-11-15 09:46:14 -0300

    Cambios en Should_Get_All_Themes y Should_Delete_A_Theme

--- test/NewsApp.Application.Tests/Themes/ThemeAppService_Test.cs
+++ test/NewsApp.Application.Tests/Themes/ThemeAppService_Test.cs
@@ -23,20 +23,17 @@ namespace NewsApp.Themes
         {
             // Arrange
             var themeCount = 3;
-            var createdThemes = new List<ThemeDto>();
 
             for (int i = 0; i < themeCount; i++)
             {
                 var themeToAdd = new CreateUpdateThemeDto
                 {
-                    // Set the properties of the theme as needed
                     Name = $"Theme{i + 1}",
                     KeyWordsToAdd = new List<string> { $"Keyword{i + 1}_1", $"Keyword{i + 1}_2" },
-                    UserId = Guid.NewGuid() // or set the user ID as needed
+                    UserId = Guid.NewGuid()
                 };
 
-                var addedTheme = await _themeAppService.CreateThemeAsync(themeToAdd);
-                createdThemes.Add(addedTheme);
+                await _themeAppService.CreateThemeAsync(themeToAdd);
             }
 
             //Act
@@ -53,8 +50,9 @@ namespace NewsApp.Themes
             // Arrange
             var themeToAdd = new CreateUpdateThemeDto // debería crear un ThemeDto y luego convertirlo a CreateUpdateThemeDto?, el problema es que me da error al hacerlo
             {
-                // Initialize the theme properties as needed
-                // For example, Name, Description, etc.
+                Name = "Theme",
+                KeyWordsToAdd = new List<string> { "Keyword_1", "Keyword_2" },
+                UserId = Guid.NewGuid()
             };
 
             var addedTheme = await _themeAppService.CreateThemeAsync(themeToAdd);
